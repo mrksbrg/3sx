5df8117dae4161fb1822011b40f83476449c2c84
Markus Borg <markus.borg@gmail.com>
2026-09-07 22:01:52 +0200

    refactor(effe5): name after-image timer predicate

--- src/sf33rd/Source/Game/effect/effe5.c
+++ src/sf33rd/Source/Game/effect/effe5.c
@@ -69,6 +69,10 @@ static s32 after_image_should_reset(WORK_Other* ewk, const PLW* mwk) {
            (ewk->wu.dir_old & 0x80 && pcon_dp_flag) || !mwk->image_setup_flag;
 }
 
+static s32 after_image_timer_expired(WORK_Other* ewk) {
+    return EXE_flag == 0 && Game_pause == 0 && --ewk->wu.dir_step <= 0;
+}
+
 
 void effect_E5_move(WORK_Other* ewk) {
     PLW* mwk = (PLW*)ewk->my_master;
@@ -140,7 +144,7 @@ void effect_E5_move(WORK_Other* ewk) {
                 /* fallthrough */
 
             case 2:
-                if (EXE_flag == 0 && Game_pause == 0 && --ewk->wu.dir_step <= 0) {
+                if (after_image_timer_expired(ewk)) {
                     effect_E7_init(ewk, mwk);
                     ewk->wu.routine_no[2] = 1;
                 }
