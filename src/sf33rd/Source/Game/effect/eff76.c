544a95e6c40e10edd1b4d529d40909cc2184a5bb
Markus Borg <markus.borg@gmail.com>
2026-09-07 21:32:47 +0200

    refactor(eff76): name English name predicate

--- src/sf33rd/Source/Game/effect/eff76.c
+++ src/sf33rd/Source/Game/effect/eff76.c
@@ -38,6 +38,11 @@ void Setup_Color_76(WORK_Other* ewk);
 void (*const EFF76_Jmp_Tbl[8])() = { EFF76_WAIT, EFF76_SLIDE_IN, EFF76_SLIDE_OUT,       EFF76_SUDDENLY,
                                      EFF57_KILL, EFF76_SHIFT,    EFF76_WAIT_BREAK_INTO, EFF76_BEFORE };
 
+static s32 uses_english_elena_name(s16 player_number) {
+    return player_number == 14 && mpp_w.language == LANG_ENGLISH;
+}
+
+
 void effect_76_move(WORK_Other* ewk) {
     EFF76_Jmp_Tbl[ewk->wu.routine_no[0]](ewk);
 
@@ -624,7 +629,7 @@ void Setup_Color_L1(WORK_Other* ewk) {
 }
 
 s32 chkNameAkuma(s32 plnum, s32 rnum) {
-    if (plnum == 14 && mpp_w.language == LANG_ENGLISH) {
+    if (uses_english_elena_name(plnum)) {
         return rnum;
     }
 
