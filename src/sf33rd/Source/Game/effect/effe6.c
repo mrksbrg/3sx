51edda73a7bc8854cca2e8af970bbb30f778119c
Markus Borg <markus.borg@gmail.com>
2026-09-08 13:17:56 +0200

    refactor(effe6): simplify effe6_0025

--- src/sf33rd/Source/Game/effect/effe6.c
+++ src/sf33rd/Source/Game/effect/effe6.c
@@ -1061,8 +1061,63 @@ void effe6_0024(WORK_Other* ewk) {
     }
 }
 
-void effe6_0025(WORK_Other* ewk) {
+static void update_effe6_0025_player_two(WORK_Other* ewk) {
     u16 work;
+
+    if (p2sw_0 & 8) {
+        ewk->wu.xyz[0].disp.pos++;
+    }
+
+    if (p2sw_0 & 4) {
+        ewk->wu.xyz[0].disp.pos--;
+    }
+
+    if (p2sw_0 & 1) {
+        ewk->wu.xyz[1].disp.pos++;
+    }
+
+    if (p2sw_0 & 2) {
+        ewk->wu.xyz[1].disp.pos--;
+    }
+
+    work = p2sw_0 & ~p2sw_1;
+
+    if (work & 0x10) {
+        char_move_z(&ewk->wu);
+        ewk->wu.old_rno[2]++;
+
+        if (ewk->wu.old_rno[2] >= 7) {
+            ewk->wu.old_rno[2] = 0;
+        }
+    }
+
+    switch (ewk->wu.old_rno[2]) {
+    case 0:
+        ewk->wu.xyz[0].disp.pos = 608;
+        break;
+
+    case 1:
+        ewk->wu.xyz[0].disp.pos = 609;
+        break;
+
+    case 2:
+        ewk->wu.xyz[0].disp.pos = 610;
+        break;
+
+    case 3:
+        ewk->wu.xyz[0].disp.pos = 607;
+        break;
+
+    case 4:
+        ewk->wu.xyz[0].disp.pos = 606;
+        break;
+
+    case 5:
+        ewk->wu.xyz[0].disp.pos = 611;
+    }
+}
+
+void effe6_0025(WORK_Other* ewk) {
 
     switch (ewk->wu.routine_no[1]) {
     case 0:
@@ -1089,57 +1144,7 @@ void effe6_0025(WORK_Other* ewk) {
                 ewk->wu.xyz[1].disp.pos--;
             }
         } else {
-            if (p2sw_0 & 8) {
-                ewk->wu.xyz[0].disp.pos++;
-            }
-
-            if (p2sw_0 & 4) {
-                ewk->wu.xyz[0].disp.pos--;
-            }
-
-            if (p2sw_0 & 1) {
-                ewk->wu.xyz[1].disp.pos++;
-            }
-
-            if (p2sw_0 & 2) {
-                ewk->wu.xyz[1].disp.pos--;
-            }
-
-            work = p2sw_0 & ~p2sw_1;
-
-            if (work & 0x10) {
-                char_move_z(&ewk->wu);
-                ewk->wu.old_rno[2]++;
-
-                if (ewk->wu.old_rno[2] >= 7) {
-                    ewk->wu.old_rno[2] = 0;
-                }
-            }
-
-            switch (ewk->wu.old_rno[2]) {
-            case 0:
-                ewk->wu.xyz[0].disp.pos = 608;
-                break;
-
-            case 1:
-                ewk->wu.xyz[0].disp.pos = 609;
-                break;
-
-            case 2:
-                ewk->wu.xyz[0].disp.pos = 610;
-                break;
-
-            case 3:
-                ewk->wu.xyz[0].disp.pos = 607;
-                break;
-
-            case 4:
-                ewk->wu.xyz[0].disp.pos = 606;
-                break;
-
-            case 5:
-                ewk->wu.xyz[0].disp.pos = 611;
-            }
+            update_effe6_0025_player_two(ewk);
         }
 
         disp_pos_trans_entry(ewk);
