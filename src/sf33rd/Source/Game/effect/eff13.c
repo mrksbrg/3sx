399cc598dee32942137f875813178673616f4dbc
Markus Borg <markus.borg@gmail.com>
2026-09-08 12:09:33 +0200

    refactor(eff13): simplify kotp_09000

--- src/sf33rd/Source/Game/effect/eff13.c
+++ src/sf33rd/Source/Game/effect/eff13.c
@@ -1251,6 +1251,54 @@ void kotp_08000(WORK_Other* ewk, TAMA* twk) {
     }
 }
 
+static void resolve_kotp_09_hit(WORK_Other* ewk, TAMA* twk) {
+    ewk->wu.vital_new -= ewk->wu.dm_vital;
+    ewk->wu.dm_vital = 0;
+
+    if (ewk->wu.vital_new < 256) {
+        if (ewk->wu.hf.hit.player) {
+            if (ewk->wu.hf.hit.player & 0xF0) {
+                set_char_move_init(&ewk->wu, 0, twk->erdf);
+            } else {
+                set_char_move_init(&ewk->wu, 0, twk->erht);
+            }
+        } else {
+            set_char_move_init(&ewk->wu, 0, twk->erex);
+        }
+
+        ewk->wu.routine_no[1] = 2;
+        ewk->wu.routine_no[2] = 1;
+        ewk->wu.kage_flag = 0;
+        ewk->wu.hit_stop = 0;
+    } else {
+        ewk->wu.routine_no[1] = 0;
+
+        if (ewk->wu.hf.hit.player) {
+            if (ewk->wu.hf.hit.player & 0xF0) {
+                set_char_move_init(&ewk->wu, 0, twk->erdf);
+            } else {
+                set_char_move_init(&ewk->wu, 0, twk->erht);
+            }
+
+            ewk->wu.routine_no[1] = 2;
+            ewk->wu.routine_no[2] = 1;
+            ewk->wu.kage_flag = 0;
+            ewk->wu.hit_stop = 0;
+        } else {
+            effect_96_init(&ewk->wu, twk->erex, ewk->wu.disp_flag, ewk->wu.hit_stop);
+        }
+
+        if (ewk->dm_refrect) {
+            ewk->master_id = (ewk->master_id + 1) & 1;
+            ewk->wu.rl_flag = (ewk->wu.rl_flag) + 1 & 1;
+            ewk->dm_refrect = 0;
+        }
+    }
+
+    ewk->wu.hf.hit_flag = 0;
+    ewk->wu.hit_quake = 0;
+}
+
 void kotp_09000(WORK_Other* ewk, TAMA* twk) {
     if (ewk->wu.hf.hit_flag) {
         ewk->wu.routine_no[1] = 1;
@@ -1298,51 +1346,7 @@ void kotp_09000(WORK_Other* ewk, TAMA* twk) {
         break;
 
     case 1:
-        ewk->wu.vital_new -= ewk->wu.dm_vital;
-        ewk->wu.dm_vital = 0;
-
-        if (ewk->wu.vital_new < 256) {
-            if (ewk->wu.hf.hit.player) {
-                if (ewk->wu.hf.hit.player & 0xF0) {
-                    set_char_move_init(&ewk->wu, 0, twk->erdf);
-                } else {
-                    set_char_move_init(&ewk->wu, 0, twk->erht);
-                }
-            } else {
-                set_char_move_init(&ewk->wu, 0, twk->erex);
-            }
-
-            ewk->wu.routine_no[1] = 2;
-            ewk->wu.routine_no[2] = 1;
-            ewk->wu.kage_flag = 0;
-            ewk->wu.hit_stop = 0;
-        } else {
-            ewk->wu.routine_no[1] = 0;
-
-            if (ewk->wu.hf.hit.player) {
-                if (ewk->wu.hf.hit.player & 0xF0) {
-                    set_char_move_init(&ewk->wu, 0, twk->erdf);
-                } else {
-                    set_char_move_init(&ewk->wu, 0, twk->erht);
-                }
-
-                ewk->wu.routine_no[1] = 2;
-                ewk->wu.routine_no[2] = 1;
-                ewk->wu.kage_flag = 0;
-                ewk->wu.hit_stop = 0;
-            } else {
-                effect_96_init(&ewk->wu, twk->erex, ewk->wu.disp_flag, ewk->wu.hit_stop);
-            }
-
-            if (ewk->dm_refrect) {
-                ewk->master_id = (ewk->master_id + 1) & 1;
-                ewk->wu.rl_flag = (ewk->wu.rl_flag) + 1 & 1;
-                ewk->dm_refrect = 0;
-            }
-        }
-
-        ewk->wu.hf.hit_flag = 0;
-        ewk->wu.hit_quake = 0;
+        resolve_kotp_09_hit(ewk, twk);
         break;
 
     case 2:
