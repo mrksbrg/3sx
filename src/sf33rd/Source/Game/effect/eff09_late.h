88f8fcbe0e16c40f4de8783a523116a0bd46136f
Markus Borg <markus.borg@gmail.com>
2026-09-09 21:27:30 +0200

    refactor(eff09): split late lifecycle module

---
+++ src/sf33rd/Source/Game/effect/eff09_late.h
@@ -0,0 +1,11 @@
+#ifndef EFF09_LATE_H
+#define EFF09_LATE_H
+
+#include "structs.h"
+
+void eff09_17000(WORK_Other* ewk);
+void eff09_18000(WORK_Other* ewk);
+void eff09_19000(WORK_Other* ewk);
+void eff09_20000(WORK_Other* ewk);
+
+#endif
