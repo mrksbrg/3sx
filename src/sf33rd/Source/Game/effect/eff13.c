d3c3e7891e05269fa8cfa4764fe86cc99c248e3e
Markus Borg <markus.borg@gmail.com>
2026-09-09 21:45:29 +0200

    refactor(eff13): deduplicate kotp_16000 finish phase

--- src/sf33rd/Source/Game/effect/eff13.c
+++ src/sf33rd/Source/Game/effect/eff13.c
@@ -1829,23 +1829,7 @@ void kotp_16000(WORK_Other* ewk, TAMA* twk) {
         break;
 
     case 2:
-        switch (ewk->wu.routine_no[2]) {
-        case 0:
-            add_mvxy_speed(&ewk->wu);
-            cal_mvxy_speed(&ewk->wu);
-            /* fallthrough */
-
-        case 1:
-            char_move(&ewk->wu);
-
-            if (ewk->wu.cg_type == 0xFF) {
-                ewk->wu.disp_flag = 0;
-                ewk->wu.routine_no[0] = 2;
-            }
-
-            break;
-        }
-
+        finish_kotp_07(ewk);
         break;
     }
 }
