978fc5f4a4fe2c607a9e0e8e5662268d2cb234e1
Markus Borg <markus.borg@gmail.com>
2026-09-09 20:46:34 +0200

    refactor(eff09): simplify eff09_15000

--- src/sf33rd/Source/Game/effect/eff09.c
+++ src/sf33rd/Source/Game/effect/eff09.c
@@ -1043,6 +1043,10 @@ void eff09_14000(WORK_Other* ewk) {
     }
 }
 
+static s32 eff09_15000_animation_updates_enabled(const WORK_Other* ewk) {
+    return !EXE_flag && !Game_pause && ewk->wu.hit_stop;
+}
+
 void eff09_15000(WORK_Other* ewk) {
     WORK* oya_ptr;
 
@@ -1063,7 +1067,7 @@ void eff09_15000(WORK_Other* ewk) {
         break;
 
     case 1:
-        if (!EXE_flag && !Game_pause && ewk->wu.hit_stop) {
+        if (eff09_15000_animation_updates_enabled(ewk)) {
             char_move(&ewk->wu);
 
             if (oya_ptr->cg_type == 9) {
