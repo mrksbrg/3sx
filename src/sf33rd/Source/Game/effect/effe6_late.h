464650e7d5295124f906ada7899fdfe07b6d8a94
Markus Borg <markus.borg@gmail.com>
2026-09-11 13:47:17 +0200

    refactor(effe6): split late state handlers

---
+++ src/sf33rd/Source/Game/effect/effe6_late.h
@@ -0,0 +1,15 @@
+#ifndef EFFE6_LATE_H
+#define EFFE6_LATE_H
+
+#include "structs.h"
+
+void effe6_0026(WORK_Other* ewk);
+void effe6_0027(WORK_Other* ewk);
+void effe6_0028(WORK_Other* ewk);
+void effe6_0029(WORK_Other* ewk);
+void effe6_0030(WORK_Other* ewk);
+void effe6_0031(WORK_Other* ewk);
+void effe6_0032(WORK_Other* ewk);
+void effe6_0033(WORK_Other* ewk);
+
+#endif
