82cfba8bef499d5ec5cc9cbe22fe0c30d15d5fc9
Markus Borg <markus.borg@gmail.com>
2026-09-11 19:10:55 +0200

    refactor(effh6): split right movement cycle

--- src/sf33rd/Source/Game/effect/effh6.c
+++ src/sf33rd/Source/Game/effect/effh6.c
@@ -49,43 +49,6 @@ static void update_h6_scroll(WORK_Other* ewk) {
     sort_push_request4(&ewk->wu);
 }
 
-static void update_h6_right_cycle(WORK_Other* ewk) {
-    switch (ewk->wu.routine_no[2]) {
-    case 0:
-        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
-        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos + roll_rate;
-        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-
-        if (ewk->wu.routine_no[5] <= ewk->wu.xyz[0].disp.pos) {
-            ewk->wu.xyz[0].disp.pos = ewk->wu.routine_no[5];
-            ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-            ewk->wu.routine_no[2]++;
-            ewk->wu.dir_timer = ewk->wu.dir_timer - 48;
-        }
-
-        break;
-
-    case 1:
-        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
-
-        if (ewk->wu.dir_timer < 1) {
-            ewk->wu.routine_no[2]++;
-        }
-
-        break;
-
-    case 2:
-        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos - roll_rate;
-        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-
-        if (ewk->wu.xyz[0].disp.pos < -127) {
-            ewk->wu.routine_no[0]++;
-        }
-
-        break;
-    }
-}
-
 static void update_h6_wait(WORK_Other* ewk) {
     ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
 
@@ -141,7 +104,7 @@ static void update_h6_direction(WORK_Other* ewk) {
         break;
 
     case 2:
-        update_h6_right_cycle(ewk);
+        effh6_update_right_cycle(ewk);
         break;
 
     case 3:
