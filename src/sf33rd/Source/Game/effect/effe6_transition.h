f2d9eb97638959ea797f790f2562b794764af7f6
Markus Borg <markus.borg@gmail.com>
2026-09-11 13:45:43 +0200

    refactor(effe6): split transition state handlers

---
+++ src/sf33rd/Source/Game/effect/effe6_transition.h
@@ -0,0 +1,15 @@
+#ifndef EFFE6_TRANSITION_H
+#define EFFE6_TRANSITION_H
+
+#include "structs.h"
+
+void effe6_0018(WORK_Other* ewk);
+void effe6_0019(WORK_Other* ewk);
+void effe6_0020(WORK_Other* ewk);
+void effe6_0021(WORK_Other* ewk);
+void effe6_0022(WORK_Other* ewk);
+void effe6_0023(WORK_Other* ewk);
+void effe6_0024(WORK_Other* ewk);
+void effe6_0025(WORK_Other* ewk);
+
+#endif
