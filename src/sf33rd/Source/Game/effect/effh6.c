46eb3cfea73373338a19bcaa1ffe0b5c347476a5
Markus Borg <markus.borg@gmail.com>
2026-09-11 19:08:48 +0200

    refactor(effh6): split left movement cycle

--- src/sf33rd/Source/Game/effect/effh6.c
+++ src/sf33rd/Source/Game/effect/effh6.c
@@ -4,6 +4,7 @@
  */
 
 #include "sf33rd/Source/Game/effect/effh6.h"
+#include "sf33rd/Source/Game/effect/effh6_cycles.h"
 #include "common.h"
 #include "sf33rd/Source/Game/effect/effect.h"
 #include "sf33rd/Source/Game/ending/end_data.h"
@@ -46,42 +47,6 @@ static void update_h6_scroll(WORK_Other* ewk) {
     }
 
     sort_push_request4(&ewk->wu);
-}
-
-static void update_h6_left_cycle(WORK_Other* ewk) {
-    switch (ewk->wu.routine_no[2]) {
-    case 0:
-        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
-        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos - roll_rate;
-        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-        if (ewk->wu.routine_no[5] >= ewk->wu.xyz[0].disp.pos) {
-            ewk->wu.xyz[0].disp.pos = ewk->wu.routine_no[5];
-            ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-            ewk->wu.routine_no[2]++;
-            ewk->wu.dir_timer = ewk->wu.dir_timer - 48;
-        }
-
-        break;
-
-    case 1:
-        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
-
-        if (ewk->wu.dir_timer < 1) {
-            ewk->wu.routine_no[2]++;
-        }
-
-        break;
-
-    case 2:
-        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos + roll_rate;
-        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-
-        if (512 <= ewk->wu.xyz[0].disp.pos) {
-            ewk->wu.routine_no[0]++;
-        }
-
-        break;
-    }
 }
 
 static void update_h6_down_cycle(WORK_Other* ewk, s16 delay) {
@@ -204,7 +169,7 @@ static void initialize_h6_move(WORK_Other* ewk) {
 static void update_h6_direction(WORK_Other* ewk) {
     switch (ewk->wu.routine_no[1]) {
     case 0:
-        update_h6_left_cycle(ewk);
+        effh6_update_left_cycle(ewk);
         break;
 
     case 1:
---
+++ src/sf33rd/Source/Game/effect/effh6_left_cycle.c
@@ -0,0 +1,37 @@
+#include "sf33rd/Source/Game/effect/effh6_cycles.h"
+
+void effh6_update_left_cycle(WORK_Other* ewk) {
+    switch (ewk->wu.routine_no[2]) {
+    case 0:
+        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
+        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos - roll_rate;
+        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
+        if (ewk->wu.routine_no[5] >= ewk->wu.xyz[0].disp.pos) {
+            ewk->wu.xyz[0].disp.pos = ewk->wu.routine_no[5];
+            ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
+            ewk->wu.routine_no[2]++;
+            ewk->wu.dir_timer = ewk->wu.dir_timer - 48;
+        }
+
+        break;
+
+    case 1:
+        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
+
+        if (ewk->wu.dir_timer < 1) {
+            ewk->wu.routine_no[2]++;
+        }
+
+        break;
+
+    case 2:
+        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos + roll_rate;
+        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
+
+        if (512 <= ewk->wu.xyz[0].disp.pos) {
+            ewk->wu.routine_no[0]++;
+        }
+
+        break;
+    }
+}
