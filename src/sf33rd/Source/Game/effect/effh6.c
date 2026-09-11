1b275bb953c8bbcfcae9dec614c8e9950afb1fe3
Markus Borg <markus.borg@gmail.com>
2026-09-11 18:48:12 +0200

    refactor(effh6): extract left movement cycle

--- src/sf33rd/Source/Game/effect/effh6.c
+++ src/sf33rd/Source/Game/effect/effh6.c
@@ -48,6 +48,42 @@ static void update_h6_scroll(WORK_Other* ewk) {
     sort_push_request4(&ewk->wu);
 }
 
+static void update_h6_left_cycle(WORK_Other* ewk) {
+    switch (ewk->wu.routine_no[2]) {
+    case 0:
+        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
+        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos - roll_rate;
+        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
+        if (ewk->wu.routine_no[5] >= ewk->wu.xyz[0].disp.pos) {
+            ewk->wu.xyz[0].disp.pos = ewk->wu.routine_no[5];
+            ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
+            ewk->wu.routine_no[2]++;
+            ewk->wu.dir_timer = ewk->wu.dir_timer - 48;
+        }
+
+        break;
+
+    case 1:
+        ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
+
+        if (ewk->wu.dir_timer < 1) {
+            ewk->wu.routine_no[2]++;
+        }
+
+        break;
+
+    case 2:
+        ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos + roll_rate;
+        ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
+
+        if (512 <= ewk->wu.xyz[0].disp.pos) {
+            ewk->wu.routine_no[0]++;
+        }
+
+        break;
+    }
+}
+
 void effect_H6_move(WORK_Other* ewk) {
     switch (ewk->wu.routine_no[0]) {
     case 0:
@@ -74,40 +110,7 @@ void effect_H6_move(WORK_Other* ewk) {
 
         switch (ewk->wu.routine_no[1]) {
         case 0:
-            switch (ewk->wu.routine_no[2]) {
-            case 0:
-                ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
-                ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos - roll_rate;
-                ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-                if (ewk->wu.routine_no[5] >= ewk->wu.xyz[0].disp.pos) {
-                    ewk->wu.xyz[0].disp.pos = ewk->wu.routine_no[5];
-                    ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-                    ewk->wu.routine_no[2]++;
-                    ewk->wu.dir_timer = ewk->wu.dir_timer - 48;
-                }
-
-                break;
-
-            case 1:
-                ewk->wu.dir_timer = ewk->wu.dir_timer - roll_rate_t;
-
-                if (ewk->wu.dir_timer < 1) {
-                    ewk->wu.routine_no[2]++;
-                }
-
-                break;
-
-            case 2:
-                ewk->wu.xyz[0].disp.pos = ewk->wu.xyz[0].disp.pos + roll_rate;
-                ewk->wu.position_x = ewk->wu.xyz[0].disp.pos & 0xFFFF;
-
-                if (512 <= ewk->wu.xyz[0].disp.pos) {
-                    ewk->wu.routine_no[0]++;
-                }
-
-                break;
-            }
-
+            update_h6_left_cycle(ewk);
             break;
 
         case 1:
